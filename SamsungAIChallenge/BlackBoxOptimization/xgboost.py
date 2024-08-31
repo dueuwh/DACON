a29f9f1f8801503c71133cc338a40902530127c7
dueuwh <usang0821@naver.com>
2024-08-31 14:28:11 +0900

    feat: xgb save

--- SamsungAIChallenge/BlackBoxOptimization/xgboost.py
+++ SamsungAIChallenge/BlackBoxOptimization/xgboost.py
@@ -1,7 +1,21 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug 31 14:12:18 2024
+import os
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import xgboost as xgb
+import itertools
 
-@author: ys
-"""
+def xgb_analysis():
+    data_path = "./data/"
+    submission_file_path = data_path + "sample_submission.csv"
+    test_file_path = data_path + "test.csv"
+    train_file_path = data_path + "train.csv"
+    
+    test_file = pd.read_csv(test_file_path, index_col=0)
+    train_file = pd.read_csv(train_file_path, index_col=0)
+    submission_file = pd.read_csv(submission_file_path, index_col=0)
 
+if __name__ == "__main__":
+    xgb_analysis()
--- SamsungAIChallenge/MachineLearningForceFields/xgboost.py
+++ SamsungAIChallenge/MachineLearningForceFields/xgboost.py
@@ -1,7 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug 31 14:12:26 2024
+import os
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import xgboost as xgb
+import itertools
 
-@author: ys
-"""
+def xgb_analysis():
+    data_path = "./data/"
+    submission_file_path = data_path + "sample_submission.csv"
+    test_file_path = data_path + "Test.xyz"
+    train_file_path = data_path + "Train.xyz"
+    
+    test_list = []
+    with open(test_file_path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            test_list.append(line)
 
+    train_list = []
+    with open(train_file_path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            train_list.append(line.strip())
+    
+    submission_file = pd.read_csv(submission_file_path, index_col=0)
+
+    print("test_list: ", test_list)
+
+if __name__ == "__main__":
+    xgb_analysis()
