c899ccb299ea3e442b276ea179bd3586ce8bdb28
dueuwh <usang0821@naver.com>
2024-08-31 14:15:36 +0900

    feat: xgboost anlysis

---
+++ SamsungAIChallenge/BlackBoxOptimization/xgboost.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 31 14:12:18 2024
+
+@author: ys
+"""
+
---
+++ SamsungAIChallenge/MachineLearningForceFields/xgboost.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 31 14:12:26 2024
+
+@author: ys
+"""
+
